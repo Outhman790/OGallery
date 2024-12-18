65bc6f7ccdc4d94a0fe7ef3c868cfe57be7afd07
Outhman Moumou <tangerino2011@gmail.com>
2024-12-18 06:39:14 +0100

    just added some comment for future improvements

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
      <Navbar />
+     {/* you have to remove the div wraping the gallery component */}
      <div className="mx-10">
      <Gallery />
      </div>
