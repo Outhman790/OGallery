ff8f2bf40efd154961509fa61d08ab7f4f70fac7
Outhman Moumou <tangerino2011@gmail.com>
2024-12-18 06:38:57 +0100

    made some style changes

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,7 +1,7 @@
 import Image from "./Image"
 const Gallery = () => { 
     return (
-        <div  className="w-full container max-w-[1200px] grid place-items-center grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-10 mt-20 pt-10 mx-auto">   
+        <div  className="w-full container max-w-[1200px] grid place-items-center grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-10 pt-10 mx-auto transition ease-in-out duration-300">   
                 {Array.from({ length: 10 }).map(() => (
                 <Image />
             ))}
