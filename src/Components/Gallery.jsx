1a5c3b2729e946b435a8625cc7ea80ab08543741
Outhman Moumou <tangerino2011@gmail.com>
2024-12-20 08:35:15 +0100

    I added code to open modal with add button

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,11 +1,30 @@
+import AddImage from "./addImage";
 import Image from "./Image"
+import { useState } from "react"
 const Gallery = () => { 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const tempArrImages = [1,2,3,4,5,6,7,8,9]
     return (
-        <div  className="w-full container max-w-[1200px] grid place-items-center grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-10 pt-10 mx-auto transition ease-in-out duration-300">   
-                {Array.from({ length: 10 }).map(() => (
-                <Image />
-            ))}
+        <>
+        <div className="flex max-w[1200px] justify-center items-center mt-10">
+        <p className="text-center p-2 font-medium md:text-xl">{`Images number is: ${tempArrImages.length}`}</p>
+        <hr className="border-l border-gray-300 h-10 mr-3" />
+        <button onClick={openModal} className="bg-indigo-600 font-sans text-white py-2 px-3 rounded ">Add an image</button>
         </div>
+        <div  className="w-full container max-w-[1200px] grid place-items-center grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-10 pt-5 mx-auto transition ease-in-out duration-300">   
+                {tempArrImages.map(() => (
+                    <Image />
+                ))}
+        </div>
+        {isModalOpen && <AddImage closeModal={closeModal} />}
+        </>
     )
 
 }
