d43f3015d56981a01736360b122288e68a19a2de
Outhman Moumou <tangerino2011@gmail.com>
2024-12-23 07:33:26 +0100

    I've made the form as a component and also I've made some changes on 'onSubmit'.

--- src/Components/addImage.jsx
+++ src/Components/addImage.jsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
-import { FaRegFileImage } from "react-icons/fa";
+import AddImageForm from "./addImageForm";
 import { IoClose } from "react-icons/io5";
-import { useForm } from "react-hook-form";
 
-const AddImage = ({ closeModal }) => {
-  const [isImage, setIsImage] = useState(true);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    trigger,
-  } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+const AddImage = ({ closeModal, items, setItems }) => {
+  const [image, setImage] = useState("");
+  const onSubmit = (formData) => {
+    const newItem = {
+      id: Date.now(),
+      name: formData.name,
+      description: formData.imageDescription,
+      image: formData.imageFile[0]
+        ? URL.createObjectURL(formData.imageFile[0])
+        : "",
+      author: "temp fake author",
+    };
+    setItems([...items, newItem]);
+    closeModal();
   };
-  const showImage = () => {
-    setIsImage(true);
-  };
-  const handleBlur = (field) => {
-    trigger(field);
-  };
+
+  // const onChange = (e) => {
+  //   e.target.files && e.target.files.length > 0
+  //     ? setImage(URL.createObjectURL(e.target.files[0]))
+  //     : "";
+  // };
 
   return (
     <div
@@ -34,101 +37,17 @@ const AddImage = ({ closeModal }) => {
           className="absolute lg:top-2 lg:right-2 top-1 right-[2px] text-2xl text-gray-400 hover:text-gray-700 hover:cursor-pointer"
           onClick={closeModal}
         />
-        {isImage && (
+        {image && (
           <div>
             <img
               className="mx-auto pt-5 md:h-[400px] object-cover max-w-[350px] lg:max-w-[400px]"
-              src="https://placehold.co/400x400"
+              src={image}
               alt="tempImg"
             />
           </div>
         )}
         <div>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col justify-center items-center gap-1 py-5 font-sans "
-          >
-            <label
-              className="cursor-pointer text-indigo-600 my-1 px-4 rounded-md"
-              htmlFor="imageName"
-            >
-              Image name:
-            </label>
-            <input
-              className="outline-indigo-600 focus:bg-indigo-100"
-              type="text"
-              id="imageName"
-              {...register("name", {
-                validate: {
-                  minLength: (value) =>
-                    value.length >= 3 ||
-                    "Name must be at least 3 characters long",
-                  noSpecialChars: (value) =>
-                    /^[A-Za-z]+$/.test(value) ||
-                    "Name must only contain letters",
-                },
-              })}
-              onBlur={() => handleBlur("name")}
-            />
-            {errors.name && (
-              <p className="text-red-500 text-[.8rem]">{errors.name.message}</p>
-            )}
-
-            <label
-              className="cursor-pointer text-indigo-600 my-1 px-4 rounded-md"
-              htmlFor="imageDescription"
-            >
-              Image description:
-            </label>
-            <textarea
-              className="outline-indigo-600 focus:bg-indigo-100"
-              id="imageDescription"
-              rows="3"
-              cols="30"
-              {...register("imageDescription", {
-                validate: {
-                  minLength: (value) =>
-                    value.length >= 10 ||
-                    "Description must be at least 10 characters long",
-                  maxLength: (value) =>
-                    value.length <= 100 ||
-                    "Description must be less than 100 characters",
-                },
-              })}
-              onBlur={() => handleBlur("imageDescription")}
-            ></textarea>
-            {errors.imageDescription && (
-              <p className="text-red-500 text-[.8rem]">
-                {errors.imageDescription.message}
-              </p>
-            )}
-
-            <label
-              className="cursor-pointer text-indigo-600 mt-2 py-2 px-3 rounded-md border-2 border-indigo-400 bg-gray-200"
-              htmlFor="imageFile"
-            >
-              Image file
-              <FaRegFileImage className="inline-block ml-2 text-2xl" />
-            </label>
-            <input
-              className="hidden"
-              type="file"
-              id="imageFile"
-              {...register("imageFile", { required: "Image file is required" })}
-            />
-            {errors.imageFile && (
-              <p className="text-red-500 text-[.8rem]">
-                {errors.imageFile.message}
-              </p>
-            )}
-
-            <button
-              className="bg-indigo-600 font-sans text-white py-1 px-4 mt-3 rounded-md"
-              type="submit"
-            >
-              ADD
-            </button>
-          </form>
+          <AddImageForm onSubmit={onSubmit} setImage={setImage} />
         </div>
       </div>
     </div>
