1be3952c87eb4e1c7bbd08adf9c24e19ec605b42
Outhman Moumou <tangerino2011@gmail.com>
2024-12-25 23:46:31 +0100

    I added custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    theme: {
+      screens: {
+        sm: "640px",
+
+        md: "868px",
+
+        lg: "1024px",
+
+        xl: "1280px",
+
+        "2xl": "1536px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["Inter", "sans-serif"],
