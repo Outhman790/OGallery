83fd65c0ff4e613f4b121998433b2af5ebb81f67
Outhman Moumou <tangerino2011@gmail.com>
2024-12-27 23:24:59 +0100

    Style changes for my profile

--- src/pages/Myprofile.jsx
+++ src/pages/Myprofile.jsx
@@ -1,4 +1,4 @@
-import { FaRegUser } from "react-icons/fa6";
+import { LuUser } from "react-icons/lu";
 import { TbLockPassword } from "react-icons/tb";
 import { LiaBirthdayCakeSolid } from "react-icons/lia";
 import { TbFileDescription } from "react-icons/tb";
@@ -7,35 +7,104 @@ import { MdOutlineEmail } from "react-icons/md";
 const Myprofile = () => {
   return (
     <>
-      <div className="mt-10">
+      <div className="mt-10 mb-16">
         <h1 className="font-heading font-bold text-indigo-600 text-4xl text-center">
           My Profile
         </h1>
         <p className="text-center text-gray-600 mt-3">
           Manage your personal profile information.
         </p>
-        <div className="border-[2px] border-indigo-600  w-fit mt-5 mx-auto">
-          <div className="bg-indigo-400 p-5 border-[2px] border-indigo-600 rounded-t-md w-full">
-            <h2>Personal Information</h2>
+        <div className="border-[1px] border-gray-200 shadow-xl w-[20rem] md:w-[40rem] mt-5 mx-auto rounded-lg">
+          <div className="bg-indigo-200 p-5 w-full mb-5">
+            <h2 className="text-indigo-600 font-bold pl-4 ">
+              Personal Information
+            </h2>
           </div>
-          <form>
-            <p>
-              <FaRegUser className="mr-2" /> First Name
-            </p>
-            <input type="text" id="firstName" />
-            <p>Last Name</p>
-            <input type="text" id="lastName" />
-            <p>Email Adress</p>
-            <input type="email" id="emailAdress" />
-            <p>Birthday</p>
-            <input type="date" id="birthDate" />
-            <div>
-              <p>Password</p>
-              <button>Change Password</button>
+          <form className="text-sm">
+            <div className="flex flex-col justify-center items-start">
+              <div className="flex justify-start items-center w-[85%] mx-auto">
+                <LuUser className="mr-[6px] inline-block text-indigo-600 text-[1rem]" />
+                <label className="font-semibold" htmlFor="firstName">
+                  First Name
+                </label>
+              </div>
+              <input
+                className="mx-auto w-[85%] border-[1px] border-gray-300 rounded-sm mb-4 mt-2 block py-1 px-2"
+                type="text"
+                id="firstName"
+                placeholder="John"
+              />
             </div>
-            <p>Description</p>
-            <input type="textarea" id="" />
-            <button type="submit">Save Changes</button>
+            <div className="flex flex-col justify-center items-start">
+              <div className="flex justify-start items-center w-[85%] mx-auto">
+                <LuUser className="mr-[6px] inline-block text-indigo-600 text-[1rem]" />
+                <label className="font-semibold" htmlFor="lastName">
+                  Last Name
+                </label>
+              </div>
+              <input
+                className="w-[85%] mx-auto border-[1px] border-gray-300 rounded-sm mb-4 mt-2 block py-1 px-2"
+                type="text"
+                id="lastName"
+                placeholder="Doe"
+              />
+            </div>
+            <div className="flex flex-col justify-center items-start">
+              <div className="flex justify-start items-center w-[85%] mx-auto">
+                <MdOutlineEmail className="mr-[6px] inline-block text-indigo-600 text-[1rem]" />
+                <label className="font-semibold" htmlFor="emailAdress">
+                  Email Adress
+                </label>
+              </div>
+              <input
+                className="w-[85%] mx-auto border-[1px] border-gray-300 rounded-sm mb-4 mt-2 block py-1 px-2"
+                type="email"
+                id="emailAdress"
+                placeholder="john@gmail.com"
+              />
+            </div>
+            <div className="flex flex-col justify-center items-start">
+              <div className="flex justify-start items-center w-[85%] mx-auto">
+                <LiaBirthdayCakeSolid className="mr-[6px] inline-block text-indigo-600 text-[1rem]" />
+                <label className="font-semibold" htmlFor="birthDate">
+                  Birthday
+                </label>
+              </div>
+              <input
+                className="w-[85%] mx-auto border-[1px] border-gray-300 rounded-sm  block py-1 px-2 mt-2"
+                type="date"
+                id="birthDate"
+              />
+            </div>
+            <div className="flex justify-between items-center w-[85%] mx-auto my-6">
+              <div className="flex justify-start items-center">
+                <TbLockPassword className="mr-[6px] inline-block text-indigo-600 text-[1rem]" />
+                <p className="inline-block font-semibold">Password</p>
+              </div>
+              <button className="border-[1px] border-gray-200 rounded-sm block py-1 px-2 text-indigo-600 text-[.75rem]">
+                Change Password
+              </button>
+            </div>
+            <div className="flex flex-col justify-center items-start">
+              <div className="flex justify-start items-center w-[85%] mx-auto">
+                <TbFileDescription className="mr-[6px] inline-block text-indigo-600 text-[1rem]" />
+                <label className="font-semibold" htmlFor="description">
+                  Description
+                </label>
+              </div>
+              <textarea
+                className="w-[85%] mx-auto border-[1px] border-gray-300 rounded-sm mb-4 mt-2 block py-1 px-2"
+                id="description"
+                rows="5"
+                placeholder="Tell us about yourself"
+              ></textarea>
+            </div>
+            <button
+              className="text-center bg-indigo-600 text-white w-[85%] mx-auto py-2 mt-3 mb-8 rounded-md block"
+              type="submit"
+            >
+              Save Changes
+            </button>
           </form>
         </div>
       </div>
