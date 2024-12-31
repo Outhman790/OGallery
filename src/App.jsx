9b3ef38fdcccd48932fefc25bb2c6a53193ea1cc
Outhman Moumou <tangerino2011@gmail.com>
2024-12-31 20:35:57 +0100

    Wrapped the whole app with a provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import Navbar from "./Components/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/signUp";
 import MyProfile from "./pages/Myprofile";
-
+import TodoProvider from "./Context/TodoContext";
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<MyProfile />} />
-      </Routes>
-    </Router>
+    <TodoProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<MyProfile />} />
+        </Routes>
+      </Router>
+    </TodoProvider>
   );
 }
 
