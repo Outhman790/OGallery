98a68483272f66d5823b87516010c34dc46d7a46
Outhman Moumou <tangerino2011@gmail.com>
2025-03-10 17:49:33 +0000

    added links to the navbar

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { FaCamera } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen(!isOpen);
-
   return (
     <nav className="container flex justify-between items-center p-4 md:p-0 mx-auto">
       {/* Logo and Brand Name */}
-      <div className="absolute inset-0 z-10 p-4 text-center bg-white w-full h-fit md:w-auto  md:static flex justify-center items-center gap-1 md:bg-transparent md:gap-1 md:m-0 md:p-0 text-purple-600">
+      <div
+        className={`absolute inset-0 z-10 p-4 text-center ${
+          window.location.pathname === "/gallery" ? "bg-purple-100" : "bg-white"
+        }  w-full h-fit md:w-auto  md:static flex justify-center items-center gap-1 md:bg-transparent md:gap-1 md:m-0 md:p-0 text-purple-600`}
+      >
         <FaCamera className="text-3xl" />
         <h1 className="text-2xl font-bold">Ogallery</h1>
       </div>
@@ -26,11 +30,21 @@ function Navbar() {
           isOpen ? "block" : "hidden"
         } md:flex md:items-center md:gap-8 text-gray-700 absolute md:static inset-0 bg-stone-50 md:bg-transparent h-fit p-5 mt-16 md:m-0`}
       >
-        <li className="w-fit mx-auto p-1 md:p-0">Explore</li>
-        <li className="w-fit mx-auto p-1 md:p-0">Features</li>
-        <li className="w-fit mx-auto p-1 md:p-0">About</li>
-        <li className="w-fit mx-auto p-1 md:p-0">Login</li>
-        <li className="w-fit mx-auto">Signup</li>
+        <li className="w-fit mx-auto p-1 md:p-0">
+          <Link to="/gallery">Explore</Link>
+        </li>
+        <li className="w-fit mx-auto p-1 md:p-0">
+          <a href="">Features</a>
+        </li>
+        <li className="w-fit mx-auto p-1 md:p-0">
+          <a href="">About</a>
+        </li>
+        <li className="w-fit mx-auto p-1 md:p-0">
+          <Link to="/login">Login</Link>
+        </li>
+        <li className="w-fit mx-auto">
+          <Link to="/signup">Signup</Link>
+        </li>
       </ul>
     </nav>
   );
