779fbb1aa90c00b4237861da96bdb14a4ad2c292
Outhman Moumou <tangerino2011@gmail.com>
2025-03-21 23:54:07 +0000

    added a temporary middlware to see coming requests

--- Server/server.js
+++ Server/server.js
@@ -32,6 +32,10 @@ if (process.env.NODE_ENV === "production") {
     })
   );
 }
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
