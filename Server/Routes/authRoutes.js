0990ddf7701743c8eef7c76686605963e89ada36
Outhman Moumou <tangerino2011@gmail.com>
2025-03-26 20:18:55 +0000

    I've added logout and me route ( which is an endpoint to get user infos )

--- Server/Routes/authRoutes.js
+++ Server/Routes/authRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { signUpUser, loginUser } = require("../Controllers/authController");
+const { authenticateUser } = require("../authMiddleware");
+const {
+  signUpUser,
+  loginUser,
+  logout,
+} = require("../Controllers/authController");
 const router = express.Router();
 
 router.post("/signup", signUpUser);
 router.post("/login", loginUser);
-
+router.post("/logout", logout);
+router.post("/me", authenticateUser, (req, res) => {
+  res.json({ user: req.user });
+});
 module.exports = router;
