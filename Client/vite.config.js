847869bd41c8df3c7f6d3633d98c86c9213bf015
Outhman Moumou <tangerino2011@gmail.com>
2025-03-26 22:15:20 +0000

    add path aliases (@/ → src/) and optimize bundles with manual chunks (react, lucide, shadcn) for performance

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          lucide: ["lucide-react"],
+          shadcn: ["@/components/ui/card", "@/components/ui/badge"],
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
