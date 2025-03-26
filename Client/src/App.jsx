ba07a9103cf06ce37f288bd3b1a6b43bebfe37ca
Outhman Moumou <tangerino2011@gmail.com>
2025-03-26 22:12:29 +0000

    Made my profile and admin dashboard as protected routes only accessible by specified roles

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,8 +4,10 @@ import Gallery from "./Components/Gallery";
 import Login from "./pages/Login";
 import Signup from "./pages/signUp";
 import MyProfile from "./pages/Myprofile";
+import AdminDashboard from "./pages/AdminDashboard";
 import GlobalProvider from "./Context/GlobalState";
 import AuthProvider from "./Context/AuthContext";
+import ProtectedRoute from "./Components/protectedRoute";
 
 function App() {
   return (
@@ -17,7 +19,22 @@ function App() {
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/profile" element={<MyProfile />} />
+            <Route
+              path="/myprofile"
+              element={
+                <ProtectedRoute allowedRoles={"user"}>
+                  <MyProfile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/adminDashboard"
+              element={
+                <ProtectedRoute allowedRoles={"admin"}>
+                  <AdminDashboard />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </Router>
       </GlobalProvider>
