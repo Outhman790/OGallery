526716ceeb80b892fdd1e930f502aff0457b2adf
Outhman Moumou <tangerino2011@gmail.com>
2025-05-22 23:43:00 +0100

    I've modified authenticateUser middleware Path since I've changed the project structure

--- Server/Routes/authRoutes.js
+++ Server/Routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { authenticateUser } = require("../authMiddleware");
+const router = express.Router();
+const { authenticateUser } = require("../Middlewares/authMiddleware");
 const {
   signUpUser,
   loginUser,
   logout,
 } = require("../Controllers/authController");
-const router = express.Router();
 
 router.post("/signup", signUpUser);
 router.post("/login", loginUser);
