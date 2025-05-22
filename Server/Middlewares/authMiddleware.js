cf61bbf8ea01529c838291e13a39dca48f046b38
Outhman Moumou <tangerino2011@gmail.com>
2025-05-22 23:43:34 +0100

    Moved authMiddleware to Middlewares folder for better project structure

---
+++ Server/Middlewares/authMiddleware.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+
+// Middleware to check authentication and extract user role
+const authenticateUser = (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("Token:", token);
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Add user info (id, role) to request object
+    next();
+  } catch (err) {
+    console.error("JWT verification failed:", err.message);
+
+    res.status(403).json({ message: "Invalid or expired token." });
+  }
+};
+
+// Middleware to check roles
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ message: "Access denied. You don’t have permission." });
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizeRoles };
