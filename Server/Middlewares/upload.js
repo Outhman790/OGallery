08f5269ee46767a2d940caa3e9429a66414da2a2
Outhman Moumou <tangerino2011@gmail.com>
2025-05-22 23:41:41 +0100

    Image uploading using Multer and multer S3

---
+++ Server/Middlewares/upload.js
@@ -0,0 +1,26 @@
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const path = require("path");
+const s3 = require("../config/s3");
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      const baseName = path.basename(file.originalname, ext);
+      const safeName = baseName.replace(/\s+/g, "-").toLowerCase();
+      const fileName = `user-${
+        req.user.id
+      }/images/${Date.now()}-${safeName}${ext}`;
+      cb(null, fileName);
+    },
+  }),
+});
+
+module.exports = upload;
