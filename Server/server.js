62811eff15c6850108fa80ae0c8e8b377adb7aa6
Outhman Moumou <tangerino2011@gmail.com>
2025-06-09 05:05:21 +0100

    refactor(server): clean up Express server to API-only setup
    Removed static React file serving which was used earlier for learning purposes.
    Frontend is now handled separately (later via AWS Amplify ), so the backend focuses purely on API routes.

--- Server/server.js
+++ Server/server.js
@@ -1,36 +1,21 @@
 const express = require("express");
-const path = require("path");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
 const authRoutes = require("./Routes/authRoutes");
 const cookieParser = require("cookie-parser");
-
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Vite default port
+    credentials: true, // important for cookies (JWT in cookies)
+  })
+);
 // Middleware to parse JSON
 app.use(express.json());
 app.use(cookieParser()); // Enable cookie parsing
 
 app.use("/", authRoutes);
 
-// Serve React static files (from `dist` folder)
-app.use(express.static(path.join(__dirname, "../Client/dist")));
-
-if (process.env.NODE_ENV === "production") {
-  // Serve React static files (production only)
-  app.use(express.static(path.join(__dirname, "../Client/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Client/dist", "index.html"));
-  });
-} else {
-  // Proxy React requests to the React development server (development only)
-  app.use(
-    createProxyMiddleware({
-      target: "http://localhost:5173/", // React development server
-      changeOrigin: true,
-    })
-  );
-}
 app.use((req, res, next) => {
   console.log(`Request received: ${req.method} ${req.url}`);
   next();
