bc8d4dd0e679faed78bc052d06f94d1c96b2e131
Outhman Moumou <tangerino2011@gmail.com>
2025-06-14 02:23:30 +0100

    feat(routes): add route to fetch all images for explore page

--- Server/Routes/imageRoutes.js
+++ Server/Routes/imageRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { authenticateUser } = require('../Middlewares/authMiddleware');
 const upload = require('../Middlewares/upload');
-const { uploadImage, getUserImages } = require('../Controllers/imageController');
+const { uploadImage, getUserImages, getAllImages } = require('../Controllers/imageController');
 
 router.post('/upload', authenticateUser, upload.single('image'), uploadImage);
 router.get('/my-images', authenticateUser, getUserImages);
-
+router.get('/all-images', authenticateUser, getAllImages);
 module.exports = router;
