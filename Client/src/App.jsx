69eed63c6134bf20c02740cf095e62cc7fc5dcfb
Outhman Moumou <tangerino2011@gmail.com>
2025-06-14 02:30:31 +0100

    feat(app): add ToastProvider for global toasts and format with Prettier

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import GlobalProvider from "./Context/GlobalState";
-import AuthProvider from "./Context/AuthContext";
-import AppRoutes from "./AppRoutes";
+import { BrowserRouter as Router } from 'react-router-dom';
+import GlobalProvider from './Context/GlobalState';
+import AuthProvider from './Context/AuthContext';
+import AppRoutes from './AppRoutes';
+import ToastProvider from './lib/ToastProvider';
 function App() {
   return (
     <AuthProvider>
       <GlobalProvider>
         <Router>
           <AppRoutes />
+          <ToastProvider />
         </Router>
       </GlobalProvider>
     </AuthProvider>
