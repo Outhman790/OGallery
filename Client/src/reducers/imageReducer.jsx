13879cbd12da47af13e09fc808ff76d6f26eb9ab
Outhman Moumou <tangerino2011@gmail.com>
2025-06-14 05:52:28 +0100

    refactor: rename appReducer to imageReducer and define image actions

---
+++ Client/src/reducers/imageReducer.jsx
@@ -0,0 +1,26 @@
+export default function imageReducer(state, action) {
+  switch (action.type) {
+    case 'GET_ALL_IMAGES':
+      return [...action.payload];
+
+    case 'ADD_IMAGE':
+      return [action.payload, ...state];
+
+    case 'APPEND_IMAGES':
+      return [...state, ...action.payload];
+
+    case 'PREPEND_IMAGES':
+      return [...action.payload, ...state];
+
+    case 'DELETE_IMAGE':
+      return state.filter((img) => img.id !== action.payload);
+
+    case 'UPDATE_IMAGE':
+      return state.map((img) =>
+        img.id === action.payload.id ? { ...img, ...action.payload } : img,
+      );
+
+    default:
+      return state;
+  }
+}
