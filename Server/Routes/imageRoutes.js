ea6b03555626d513c1f2b864d7ff4d339af9661a
Outhman Moumou <tangerino2011@gmail.com>
2025-06-21 22:44:47 +0100

    feat(routes): add /new-images endpoint and rename getAllImages to getImages

--- Server/Routes/imageRoutes.js
+++ Server/Routes/imageRoutes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { authenticateUser } = require('../Middlewares/authMiddleware');
 const upload = require('../Middlewares/upload');
-const { uploadImage, getUserImages, getAllImages } = require('../Controllers/imageController');
+const {
+  uploadImage,
+  getUserImages,
+  getImages,
+  getNewImages,
+} = require('../Controllers/imageController');
 
 router.post('/upload', authenticateUser, upload.single('image'), uploadImage);
 router.get('/my-images', authenticateUser, getUserImages);
-router.get('/all-images', getAllImages);
+router.get('/all-images', getImages);
+router.get('/new-images', getNewImages);
 module.exports = router;
